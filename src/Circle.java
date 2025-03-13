23928c8dbc5ec39287a58be94abb9a7c3d8fa443
Phạm Tuấn Anh <tuananhpvpl@gmail.com>
2025-03-13 22:18:10 +0700

    Homework1
    Hw01

--- src/Circle.java
+++ src/Circle.java
@@ -1,5 +1,14 @@
-public class HW01 {
-    public static void main(String[] args) {
-        
+public class Circle {
+    private float r ;
+    final float pi = 3.14f;
+
+    public Circle(float r) {
+        this.r = r;
+    }
+    public void Chuvi(){
+        System.out.println("Chuvi: " + (r*2*pi));
+    }
+    public void Dientich(){
+        System.out.println("Dientich: "+ (r*r*pi));
     }
 }
