0106e2cb6b680fde0358cecd8b097e5a7440e832
Phạm Tuấn Anh <tuananhpvpl@gmail.com>
2025-03-19 14:22:21 +0700

    TuanAnh
    Done_oop

--- src/Fraction.java
+++ src/Fraction.java
@@ -57,4 +57,22 @@ public class Fraction {
         System.out.println("Phân số: " + numerator + "/" + denominator);
     }
 
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+
 }
